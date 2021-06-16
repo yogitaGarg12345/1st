1529b3ef2105ca819de46f0d232dabc715659fd9
yogitaGarg12345 <75129225+yogitaGarg12345@users.noreply.github.com>
2021-06-15 23:08:43 -0700

    Add files via upload

---
+++ 1st/sketch.js
@@ -0,0 +1,63 @@
+function setup() {
+	createCanvas(1300, 2200);
+  
+  
+	  
+  }
+  
+  
+  function draw() {
+	background(150);
+  
+   
+	
+  
+  
+  
+  
+  
+  textSize(15);
+	fill("blue");
+	
+
+ 
+  text("wall remains as people have been stealing from it. It was much farther north than the current wall. The current wall was built during the Ming",100,320);
+  text("built between 226–200 BC by the first Emperor of China, Qin Shi Hong (Qin Pronounced as Chin) , during the Qin Dynasty. Not much of this",100,300);
+  text("Dynasty.",100,340)
+  text("Nineteen walls have been built that were called the Great Wall of China. The first was built in the 7th century BC. The most famous wall was",100,280);
+  text("kilometers long, 9.1 metres (30 feet) wide and 15 metres high. The earlier sections on the wall are made of compacted dirt and stone. Later",100,240);
+  text("meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built. It is about 21,196",100,220)
+  text("The Great Wall of China is an ancient wall in China. The wall is made of cement, rocks, bricks, and dirt. It was finished in 1878[1] and it was",100,200)
+ 
+  text("in the Ming Dynasty they used bricks. There are 7,000 watch towers, block houses for soldiers and beacons to send smoke signals.",100,260);
+  
+  
+  
+  
+  
+  
+  textSize(15);
+  fill("blue");
+  text("The First Emperor of China, Qin Shi Huang also called Shi Huangdi, started the Qin Dynasty. The Xiongnu tribes in the north of China were",100,440);
+  text("his enemies. The land in some parts of China is easy to cross, so Qin Shi Huang started building the Great Wall to make it more difficult for",100,460);
+  text("the Xiongnu to invade China.",100,480);
+
+  text("Other dynasties in China had worked more on the wall and made it longer. The Han, Sui, Northern and Jin Dynasties all repaired, rebuilt or",100,520)
+
+
+  textSize(20)
+  fill("black")
+  text("History",100,400)
+  text("Introduction",100,150)
+  
+  textSize(50);
+  fill("red");
+   text("The Great Wall of China ",300,50);
+	drawSprites();
+   
+  }
+
+
+
+
+
